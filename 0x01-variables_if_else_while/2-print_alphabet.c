d9a2f4f9a93d51afc6eeaa39b5513305389b58a9
MaMasoud <hpdhooda@gmail.com>
2023-06-17 04:50:44 -0700

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+		putchar(10);
+	}
+	return (0);
+}
