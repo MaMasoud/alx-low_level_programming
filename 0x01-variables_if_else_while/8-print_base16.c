91bf9a8c8b4963dead67573687e5f533ab0b4465
MaMasoud <hpdhooda@gmail.com>
2023-06-18 16:26:11 -0700

    add file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 102; i++)
+{
+putchar(i);
+
+if (i == 57)
+{
+i = 96;
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
