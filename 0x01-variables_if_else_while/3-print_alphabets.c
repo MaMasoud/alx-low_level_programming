318c618cdfdb12b59b614b81d66705dc2c29b822
MaMasoud <hpdhooda@gmail.com>
2023-06-18 15:49:54 -0700

    add file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
